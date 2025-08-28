4821c8e03418a9e38ec4eca82e3aec57459171e6
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-08-28 11:47:27 +0600

    changes in lib/getProducts.js

--- src/lib/getProducts.js
+++ src/lib/getProducts.js
@@ -1,14 +1,34 @@
-// import dbConnect, { collectionNamesObj } from "@/lib/dbConnect";
+// // import dbConnect, { collectionNamesObj } from "@/lib/dbConnect";
+
+// import dbConnect, { collectionNamesObj } from "./dbConnect";
+
+// export async function getAllProducts() {
+// 	const collection = dbConnect(collectionNamesObj.productsCollection);
+// 	const products = await collection.find({}).toArray();
+
+// 	// Convert _id to string for frontend
+// 	return products.map((p) => ({
+// 		...p,
+// 		_id: p._id.toString(),
+// 	}));
+// }
 
 import dbConnect, { collectionNamesObj } from "./dbConnect";
 
 export async function getAllProducts() {
-  const collection = dbConnect(collectionNamesObj.productsCollection);
-  const products = await collection.find({}).toArray();
+	try {
+		const collection = await dbConnect(
+			collectionNamesObj.productsCollection
+		); // await lagbe যদি dbConnect async হয়
+		const products = await collection.find({}).toArray();
 
-  // Convert _id to string for frontend
-  return products.map(p => ({
-    ...p,
-    _id: p._id.toString()
-  }));
+		// Convert _id to string for frontend
+		return products.map((p) => ({
+			...p,
+			_id: p._id.toString(),
+		}));
+	} catch (error) {
+		console.error("Failed to fetch products:", error);
+		return [];
+	}
 }
